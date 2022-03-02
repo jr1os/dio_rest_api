294d2253685e7ae1408203e96da64727346638c5
Ali Rios <ali.rios@gmail.com>
2022-03-02 18:00:54 -0300

    :art: refactoring app

--- app.py
+++ app.py
@@ -1,46 +1,44 @@
-from dataclasses import dataclass
+from crypt import methods
 from flask import Flask, jsonify, request
 import json
 
 
 app = Flask(__name__)
 
+tasks = [
+    {
+        "id": 0,
+        "responsible": "ali rios",
+        "task": "create page html",
+        "status": "not started"},
+    {
+        "id": 1,
+        "responsible": "jonathan torres",
+        "task": "upgrade page javascript",
+        "status": "pending"
+    }]
 
-developers = [
-    {'id': 0,
-    'name': 'ali',
-    'skill': ['Python', "Golang", "Javascript"]
-    },
-    {'id': 1,
-    'name': 'jonathan',
-    'skill': ['javascript', 'flask', 'django']}
- ]
-
-
-@app.route('/dev/<int:id>/', methods=['GET','DELETE','PUT'])
-def developer(id):
-    if request.method == 'GET':
+@app.route("/task/<int:id>/", methods=["GET", "PUT", "DELETE"])
+def task(id):
+    if request.method == "GET":
         try:
-            resp = developers[id]
+            resp = tasks[id]
         except IndexError:
-            resp = {'status':'erro', 'message':f'developer {id} not exist'}
-        return jsonify(resp)
-    elif request.method == 'PUT':
+            resp = {"status": "error", "message": f"id {id} not exist"}
+        return resp
+    elif request.method == "PUT":
         dat = json.loads(request.data)
-        developers[id] = dat
-        return jsonify(dat)        
-    elif request.method == 'DELETE':
-        developers.pop(id)
-        return jsonify({"status":"success"})
+        tasks[id] = dat
+        return jsonify(dat)
+    elif request.method == "DELETE":
+        tasks.pop(id)
+        return jsonify({"status": "sucess", "message": f"id {id} deleted"})
 
-
-
-@app.route('/dev/', methods=['GET', 'POST'])
-def list_developer():
-    if request.method == 'POST':
+@app.route("/task/", methods=["GET", "POST"])
+def list_task():
+    if request.method == "POST":
         dat = json.loads(request.data)
-        developers.append(dat)
-        return jsonify({'message': 'developer added'})
-    elif request.method == 'GET':
-        return jsonify(developers)
-
+        tasks.append(dat)
+        return jsonify({"status":"task added"})
+    elif request.method == "GET":
+        return jsonify(tasks)
